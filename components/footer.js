c23115601487569ce61bb7e65622010334888e66
dragivko <63957625+dragivko@users.noreply.github.com>
2021-03-27 20:31:27 -0400

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -6,17 +6,17 @@ export default function Footer() {
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            MB Immigration Law - Your Door to Canada.
+            Journey. Immigration Law Firm - Your Door to Canada.
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href="https://www.datocms.com/docs/next-js"
+              href="https://www.journeyimmigrationlaw.com/"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
               Read Documentation
             </a>
             <a
-              href="https://github.com/datocms/nextjs-demo"
+              href="https://www.journeyimmigrationlaw.com/"
               className="mx-3 font-bold hover:underline"
             >
               View on GitHub
