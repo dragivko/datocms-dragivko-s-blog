0179f9dbe384f2767f2182df57a8944ffb3aad06
dragivko <63957625+dragivko@users.noreply.github.com>
2021-03-27 20:32:08 -0400

    Update Footer

--- components/footer.js
+++ components/footer.js
@@ -13,7 +13,7 @@ export default function Footer() {
               href="https://www.journeyimmigrationlaw.com/"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
-              Read Documentation
+              Start Here
             </a>
             <a
               href="https://www.journeyimmigrationlaw.com/"
